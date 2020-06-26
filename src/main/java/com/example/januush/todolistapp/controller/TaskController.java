5a8a43cdbb319e8a608b0a331ccaa72ecc1e5b82
januush <42897654+januush@users.noreply.github.com>
2020-06-26 14:17:53 +0200

    HATEOAS for RepositoryRestController

--- src/main/java/com/example/januush/todolistapp/controller/TaskController.java
+++ src/main/java/com/example/januush/todolistapp/controller/TaskController.java
@@ -1,9 +1,17 @@
 package com.example.januush.todolistapp.controller;
 
+import com.example.januush.todolistapp.model.Task;
 import com.example.januush.todolistapp.model.TaskRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import java.util.List;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RepositoryRestController
 class TaskController {
@@ -14,4 +22,12 @@ class TaskController {
         this.repository = repository;
     }
 
+    @GetMapping(value = "/tasks",params = {"!sort", "!page", "!size"}) // called instead of findAll from Repository when no other params are specified
+    public @ResponseBody ResponseEntity<?> readAllTasks() {
+        List<Task> tasks = repository.findAll();
+        CollectionModel<Task> resources = new CollectionModel<>(tasks);
+        resources.add(linkTo(methodOn(TaskController.class).readAllTasks()).withSelfRel());
+        logger.warn("Exposing all the tasks");
+        return ResponseEntity.ok(resources);
+    }
 }
