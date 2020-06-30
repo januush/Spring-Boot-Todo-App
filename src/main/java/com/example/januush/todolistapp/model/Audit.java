5ad3082eea4c33e54e5cd58963a3b5c10766dae6
januush <42897654+januush@users.noreply.github.com>
2020-06-30 17:57:39 +0200

    Composition with @Embeddable

--- src/main/java/com/example/januush/todolistapp/model/Audit.java
+++ src/main/java/com/example/januush/todolistapp/model/Audit.java
@@ -1,12 +1,12 @@
 package com.example.januush.todolistapp.model;
 
-import javax.persistence.MappedSuperclass;
+import javax.persistence.Embeddable;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
 import java.time.LocalDateTime;
 
-@MappedSuperclass
-abstract class BaseAuditableEntity {
+@Embeddable
+public class Audit {
     private LocalDateTime createdOn;
     private LocalDateTime updatedOn;
 
