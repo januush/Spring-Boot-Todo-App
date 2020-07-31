c73aa6c388a2a8da8065dd2473eb4cc6ee3cef85
januush <42897654+januush@users.noreply.github.com>
2020-07-31 18:36:03 +0200

    MVC bind template with ProjectWriteModel

--- src/main/java/com/example/januush/todolistapp/controller/ProjectController.java
+++ src/main/java/com/example/januush/todolistapp/controller/ProjectController.java
@@ -1,6 +1,8 @@
 package com.example.januush.todolistapp.controller;
 
+import com.example.januush.todolistapp.model.projection.ProjectWriteModel;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,7 +10,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/projects")
 class ProjectController {
     @GetMapping
-    String showProjects() {
+    String showProjects(Model model) {
+        var projectToEdit = new ProjectWriteModel();
+        projectToEdit.setDescription("test");
+        model.addAttribute("project", projectToEdit);
         return "projects"; // When returning String, Spring is looking if there is any template
     }
 }
