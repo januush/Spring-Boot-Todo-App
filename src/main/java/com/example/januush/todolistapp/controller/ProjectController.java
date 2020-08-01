ffb91f9df68c3b16a59794f1ac9c7d7f8035b2dc
januush <42897654+januush@users.noreply.github.com>
2020-08-01 14:57:50 +0200

    Add project validation and show error

--- src/main/java/com/example/januush/todolistapp/controller/ProjectController.java
+++ src/main/java/com/example/januush/todolistapp/controller/ProjectController.java
@@ -6,11 +6,13 @@ import com.example.januush.todolistapp.model.ProjectStep;
 import com.example.januush.todolistapp.model.projection.ProjectWriteModel;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -29,7 +31,14 @@ class ProjectController {
     }
 
     @PostMapping
-    String addProject(@ModelAttribute("project") ProjectWriteModel currentProjectModel, Model model) {
+    String addProject(
+            @ModelAttribute("project") @Valid ProjectWriteModel currentProjectModel,
+            BindingResult bindingResult,
+            Model model
+    ) {
+        if (bindingResult.hasErrors()) {
+            return "projects";
+        }
         // after adding we must have new ProjectWriteModel
         service.save(currentProjectModel);
         model.addAttribute("project", new ProjectWriteModel());
