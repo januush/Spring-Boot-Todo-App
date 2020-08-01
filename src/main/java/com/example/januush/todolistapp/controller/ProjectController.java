57b9bee294af0cdeaf3530b67a049845e6a0bad7
januush <42897654+januush@users.noreply.github.com>
2020-08-01 12:54:20 +0200

    Save new project with html form

--- src/main/java/com/example/januush/todolistapp/controller/ProjectController.java
+++ src/main/java/com/example/januush/todolistapp/controller/ProjectController.java
@@ -1,5 +1,6 @@
 package com.example.januush.todolistapp.controller;
 
+import com.example.januush.todolistapp.logic.ProjectService;
 import com.example.januush.todolistapp.model.ProjectStep;
 import com.example.januush.todolistapp.model.projection.ProjectWriteModel;
 import org.springframework.stereotype.Controller;
@@ -12,10 +13,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/projects")
 class ProjectController {
+    private final ProjectService service;
+
+    ProjectController(final ProjectService service) {
+        this.service = service;
+    }
+
     @GetMapping
     String showProjects(Model model) {
         model.addAttribute("project", new ProjectWriteModel());
         return "projects"; // When returning String, Spring is looking if there is any template
+    }
+
+    @PostMapping
+    String addProject(@ModelAttribute("project") ProjectWriteModel currentProjectModel, Model model) {
+        // after adding we must have new ProjectWriteModel
+        service.save(currentProjectModel);
+        model.addAttribute("project", new ProjectWriteModel());
+        model.addAttribute("message", "Dodano projekt!");
+        return "projects";
+
     }
 
     @PostMapping(params = "addStep")
