bcb971319eb5f99838403f13d685e61b18ccd2fd
januush <42897654+januush@users.noreply.github.com>
2020-08-01 10:20:55 +0200

    Handle addStep button in ProjectController

--- src/main/java/com/example/januush/todolistapp/controller/ProjectController.java
+++ src/main/java/com/example/januush/todolistapp/controller/ProjectController.java
@@ -1,9 +1,12 @@
 package com.example.januush.todolistapp.controller;
 
+import com.example.januush.todolistapp.model.ProjectStep;
 import com.example.januush.todolistapp.model.projection.ProjectWriteModel;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -13,5 +16,11 @@ class ProjectController {
     String showProjects(Model model) {
         model.addAttribute("project", new ProjectWriteModel());
         return "projects"; // When returning String, Spring is looking if there is any template
+    }
+
+    @PostMapping(params = "addStep")
+    String addProjectStep(@ModelAttribute("project") ProjectWriteModel currentProjectModel) {
+        currentProjectModel.getSteps().add(new ProjectStep());
+        return "projects";
     }
 }
