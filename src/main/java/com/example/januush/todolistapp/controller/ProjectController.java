662b0c7708f46915104c6c5ab683570666edeca7
januush <42897654+januush@users.noreply.github.com>
2020-08-01 18:03:47 +0200

    Handle deadline in ProjectController

--- src/main/java/com/example/januush/todolistapp/controller/ProjectController.java
+++ src/main/java/com/example/januush/todolistapp/controller/ProjectController.java
@@ -4,15 +4,14 @@ import com.example.januush.todolistapp.logic.ProjectService;
 import com.example.januush.todolistapp.model.Project;
 import com.example.januush.todolistapp.model.ProjectStep;
 import com.example.januush.todolistapp.model.projection.ProjectWriteModel;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Controller
@@ -50,6 +49,22 @@ class ProjectController {
     @PostMapping(params = "addStep")
     String addProjectStep(@ModelAttribute("project") ProjectWriteModel currentProjectModel) {
         currentProjectModel.getSteps().add(new ProjectStep());
+        return "projects";
+    }
+
+    @PostMapping("/{id}")
+    String createGroup(
+            @ModelAttribute("project") ProjectWriteModel currentProjectWriteModel,
+            Model model,
+            @PathVariable int id,
+            @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm") LocalDateTime deadline
+    ) {
+        try {
+            service.createGroup(deadline, id);
+            model.addAttribute("message", "Dodano grupę!");
+        } catch (IllegalStateException | IllegalArgumentException e) {
+            model.addAttribute("message", "Błąd podczas tworzenia grupy!");
+        }
         return "projects";
     }
 
