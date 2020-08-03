4182fdb97cecfe7995420ce4cf1172ff368c4091
januush <42897654+januush@users.noreply.github.com>
2020-08-03 20:14:05 +0200

    Cusom metrics with new Aspect

--- src/main/java/com/example/januush/todolistapp/aspect/LogicAspect.java
+++ src/main/java/com/example/januush/todolistapp/aspect/LogicAspect.java
@@ -1,14 +1,33 @@
 package com.example.januush.todolistapp.aspect;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Timer;
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 class LogicAspect {
+    private final Timer projectCreateGroupTimer;
+
+    public LogicAspect(final MeterRegistry registry) {
+        projectCreateGroupTimer = registry.timer("logic.project.create.group");
+    }
+
     // here we define diffrent aspects of objects, methods
+    @Around("execution(* com.example.januush.todolistapp.logic.ProjectService.createGroup(..))")
     Object aroundProjectCreateGroup(ProceedingJoinPoint jp) {
-        return null;
+        return projectCreateGroupTimer.record(() -> {
+            try {
+                return jp.proceed();
+            } catch (Throwable e) {
+                if (e instanceof RuntimeException) {
+                    throw  (RuntimeException) e;
+                }
+                throw new RuntimeException(e);
+            }
+        });
     }
 }
