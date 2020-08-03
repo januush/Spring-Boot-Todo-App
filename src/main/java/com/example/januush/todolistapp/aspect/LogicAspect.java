e9bd8744945e50b47c0a12eecb9d174a6bb9547d
januush <42897654+januush@users.noreply.github.com>
2020-08-03 20:31:44 +0200

    Pointcut

--- src/main/java/com/example/januush/todolistapp/aspect/LogicAspect.java
+++ src/main/java/com/example/januush/todolistapp/aspect/LogicAspect.java
@@ -2,22 +2,37 @@ package com.example.januush.todolistapp.aspect;
 
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.Timer;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 class LogicAspect {
+    private static final Logger logger = LoggerFactory.getLogger(LogicAspect.class);
     private final Timer projectCreateGroupTimer;
 
     public LogicAspect(final MeterRegistry registry) {
         projectCreateGroupTimer = registry.timer("logic.project.create.group");
     }
 
+    @Pointcut("execution(* com.example.januush.todolistapp.logic.ProjectService.createGroup(..))")
+    static void projectServiceCreateGroup() {
+    }
+
+    @Before("projectServiceCreateGroup()")
+    void logMethodCall(JoinPoint jp) {
+        logger.info("Before {} with {}", jp.getSignature().getName(), jp.getArgs());
+    }
+
     // here we define diffrent aspects of objects, methods
-    @Around("execution(* com.example.januush.todolistapp.logic.ProjectService.createGroup(..))")
+    @Around("projectServiceCreateGroup()")
     Object aroundProjectCreateGroup(ProceedingJoinPoint jp) {
         return projectCreateGroupTimer.record(() -> {
             try {
