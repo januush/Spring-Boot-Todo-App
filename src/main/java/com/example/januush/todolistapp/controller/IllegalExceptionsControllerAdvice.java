ad71c999a0dbf94e437e13499cdd51df86dbe4bc
januush <42897654+januush@users.noreply.github.com>
2020-08-03 18:36:55 +0200

    Handle exceptions in @ControllerAdvice component

---
+++ src/main/java/com/example/januush/todolistapp/controller/IllegalExceptionsControllerAdvice.java
@@ -0,0 +1,18 @@
+package com.example.januush.todolistapp.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+class IllegalExceptionsControllerAdvice {
+    @ExceptionHandler(IllegalArgumentException.class)
+    ResponseEntity<String> handleIllegalArgument(IllegalArgumentException e) {
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(IllegalStateException.class)
+    ResponseEntity<String> handleIllegalArgument(IllegalStateException e) {
+        return ResponseEntity.badRequest().body(e.getMessage());
+    }
+}
--- src/main/java/com/example/januush/todolistapp/controller/TaskGroupController.java
+++ src/main/java/com/example/januush/todolistapp/controller/TaskGroupController.java
@@ -95,14 +95,4 @@ class TaskGroupController {
 		taskGroupService.toggleGroup(id);
 		return ResponseEntity.noContent().build();
 	}
-
-	@ExceptionHandler(IllegalArgumentException.class)
-    ResponseEntity<String> handleIllegalArgument(IllegalArgumentException e) {
-	    return ResponseEntity.notFound().build();
-    }
-
-    @ExceptionHandler(IllegalStateException.class)
-    ResponseEntity<String> handleIllegalArgument(IllegalStateException e) {
-        return ResponseEntity.badRequest().body(e.getMessage());
-    }
 }
